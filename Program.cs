5f14ba447adaca86a0672d93282dd2890bc7529a
GabrielTAlberton <gtalberton@gmail.com>
2024-06-04 16:24:37 -0300

    working good

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Start(6);
+        Menu();
     }
 
     static void Menu()
@@ -11,12 +11,37 @@
         Console.WriteLine("***** Cronometro *****\n");
         Console.WriteLine("S = Segundos => 10s = 10 segundos");
         Console.WriteLine("M = Minuto => 10m = 10 minutos");
-        Console.WriteLine("0 = Sair\n");
+        Console.WriteLine("0s = Sair\n");
         Console.WriteLine("Quanto tempo deseja contar?");
 
         string timeChosen = Console.ReadLine().ToLower();
+        
+        int time = int.Parse(timeChosen.Substring(0, timeChosen.Length-1));        
+        char type = char.Parse(timeChosen.Substring(timeChosen.Length-1, 1));
+        
+        int multiplier = 1;
+
+        if (time == 0)
+            System.Environment.Exit(0);
+        if (type == 'm')
+            multiplier = 60;
+        
+        PreStart(time * multiplier);
+        
 
     }
+
+    static void PreStart(int time)
+{
+    Console.Clear();
+    Console.WriteLine("Ready...");
+    Thread.Sleep(1000);
+    Console.WriteLine("Set...");
+    Thread.Sleep(1000);
+    Console.WriteLine("GO!");
+    Thread.Sleep(1000);
+    Start(time);
+}
 
     static void Start(int time)
     {
@@ -32,12 +57,8 @@
 
         Console.Clear();
         Console.WriteLine($"Contrometro finalizado, tempo contado: {currentTime}. Retornando ao menu....");
-        Thread.Sleep(5000);
+        Thread.Sleep(3000);
+        Menu();
     }
-
-
-
-
-
 
 }
